8199491a660065d47cdc4bbdf6e1c80ac39c93d4
edu-well <108446261+edu-well@users.noreply.github.com>
2025-06-17 20:29:46 +0200

    Update data1.js

--- dataew/data1.js
+++ dataew/data1.js
@@ -11,3 +11,103 @@
                     keywords: ["complexes", "maths", "algèbre", "géométrie"],
                     resources: [
                        
+
+
+    {
+        type: "pdf",
+        title: "Physique Devoir de controle 1- 2012 (Enonce)",
+        url: "https://drive.google.com/file/d/1KVDD17gomxMD20w_NtaD0B7MP-a880Oq/preview"
+    },
+    {
+        type: "pdf",
+        title: "Physique Devoir de controle 1- 2012 (Correction)",
+        url: "https://drive.google.com/file/d/1JAi-qrHN6aZnughOZt7GM9V-4A0JDtAO/preview"
+    },
+    {
+        type: "pdf",
+        title: "Physique Devoir de controle 1- 2014 (Enonce)",
+        url: "https://drive.google.com/file/d/1XJqZZV6XCGzu7mEa4r4wtarnJhR579yc/preview"
+    },
+    {
+        type: "pdf",
+        title: "Physique Devoir de controle 1- 2014 (Correction)",
+        url: "https://drive.google.com/file/d/1-Xzf-BZ2Ec_bvGp2SBU6AcfU_4XexjgP/preview"
+    },
+    {
+        type: "pdf",
+        title: "Physique Devoir de controle 1- 2015 (Enonce)",
+        url: "https://drive.google.com/file/d/13t2dXMP08jtqF-GJWu1-IfbE7sZQVsRM/preview"
+    },
+    {
+        type: "pdf",
+        title: "Physique Devoir de controle 1- 2015 (Correction)",
+        url: "https://drive.google.com/file/d/1n6KQ35ZAVip2uf3GDVOAt6uq3GaeFVgH/preview"
+    },
+    {
+        type: "pdf",
+        title: "Physique Devoir de controle 1- 2016 (Enonce)",
+        url: "https://drive.google.com/file/d/1-jXEXFwntDvKswrTgPfYfG30iJIcXLwC/preview"
+    },
+    {
+        type: "pdf",
+        title: "Physique Devoir de controle 1- 2016 (Correction)",
+        url: "https://drive.google.com/file/d/1xmoiMmJ424gFLy4Q7B6CJDHVjWdM2eEp/preview"
+    },
+    {
+        type: "pdf",
+        title: "Physique Devoir de controle 1- 2017 (Enonce)",
+        url: "https://drive.google.com/file/d/177NpDsNZHbjSDYejTMEtxGLYHOWqWJvj/preview"
+    },
+    {
+        type: "pdf",
+        title: "Physique Devoir de controle 1- 2017 (Correction)",
+        url: "https://drive.google.com/file/d/1jmY9iBETiDBapr7mhAvNjdG_JutftqF2/preview"
+    },
+    {
+        type: "pdf",
+        title: "Physique Devoir de controle 1- 2018 (Enonce)",
+        url: "https://drive.google.com/file/d/1bUCHUEqiYBRogDE5DBiHxVhJe5mKBlId/preview"
+    },
+    {
+        type: "pdf",
+        title: "Physique Devoir de controle 1- 2018 (Correction)",
+        url: "https://drive.google.com/file/d/1B-yokSZ_OP_kdemFz3nh1-wKcTHZvxNJ/preview"
+    },
+    {
+        type: "pdf",
+        title: "Physique Devoir de controle 1- 2019 (Enonce)",
+        url: "https://drive.google.com/file/d/1RUIEahG0yigKUsZXQ2U8ojJFneuQdnx0//preview"
+    },
+    {
+        type: "pdf",
+        title: "Physique Devoir de controle 1- 2019 (Correction)",
+        url: "https://drive.google.com/file/d/11Rj2rvcX7_rt2XlfOE4QNGa1mpwzf69M/preview"
+    },
+    {
+        type: "pdf",
+        title: "Physique Devoir de controle 1- 2020 (Enonce)",
+        url: "https://drive.google.com/file/d/1KVDD17gomxMD20w_NtaD0B7MP-a880Oq/preview"
+    },
+    {
+        type: "pdf",
+        title: "Physique Devoir de controle 1- 2020 (Correction)",
+        url: "https://drive.google.com/file/d/1lP-JLkHzXxOiTQezoBjb-eYe01m1VIQ/preview"
+    },
+    {
+        type: "pdf",
+        title: "Physique Devoir de controle 1- 2021 (Enonce)",
+        url: "https://drive.google.com/file/d/1LL2ilCD2zezZcEec5eeOLFb1f7K1RBT6/preview"
+    },
+    {
+        type: "pdf",
+        title: "Physique Devoir de controle 1- 2021 (Correction)",
+        url: "https://drive.google.com/file/d/1x8XJWJqaNMhlhQKZqLjqXZCeZa0sXNis/preview"
+    },
+
+
+
+
+
+                              ]
+                }
+            ];
