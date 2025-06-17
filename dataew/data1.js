ca1063e47f94a16360e2dfe15c35ecff128741e6
edu-well <108446261+edu-well@users.noreply.github.com>
2025-06-17 20:32:08 +0200

    Update data1.js

--- dataew/data1.js
+++ dataew/data1.js
@@ -2,10 +2,10 @@
             const educationalResources = [
                 {
                     id: 1,
-                    title: "Mathématiques: Nombres Complexes",
+                    title: "Physique : Devoir de contrôle 1 avec Correction ",
                     description: "Cours complet sur les nombres complexes avec exercices corrigés. Ce chapitre couvre les bases des nombres complexes, leur représentation géométrique et leurs applications en physique.",
-                    type: "course",
-                    subject: "Mathématiques",
+                    type: "devoir contrôle",
+                    subject: "Physique",
                     author: "Prof. Jean Dupont",
                     date: "2023-05-15",
                     keywords: ["complexes", "maths", "algèbre", "géométrie"],
