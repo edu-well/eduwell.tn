dd2e5d2efd7f9121288325bfb5327498809d6367
EduWell <108446261+edu-well@users.noreply.github.com>
2025-09-19 06:50:59 +0200

    Update link.js

--- QuizQcm/assets/js/link.js
+++ QuizQcm/assets/js/link.js
@@ -1,40 +1,38 @@
-
-            // Test links configuration for each focus area with enhanced icons and descriptions
-            const testLinks = {
-                "Reproduction Humaine": [
-                    { name: "Pack 1 (Défi SVT): Chez l'Homme", link: "seek1" },
-                    { name: "Pack 2 (Défi SVT): Chez l'Homme", link: "#" },
-                    { name: "Pack 1 (Défi SVT): Chez la Femme", link: "#" },
-                    { name: "Pack 2 (Défi SVT): Chez la Femme", link: "#" },
-                    { name: "Pack 1 (Défi SVT): Procréation", link: "#" },
-                    { name: "Pack 2 (Défi SVT): Procréation", link: "#" }
-                ],
-                "Evolution Biologique": [
-                    { name: "Pack 1 (Défi SVT): Arguments", link: "#" },
-                    { name: "Pack 2 (Défi SVT): Arguments", link: "#" },
-                    { name: "Pack 1 (Défi SVT): Mécanismes", link: "#" },
-                    { name: "Pack 2 (Défi SVT): Mécanismes", link: "#" }
-                ],
-                "Genetique": [ 
-                    { name: "Pack 1 (Défi SVT): Brassage de l'information Génétique", link: "#" },
-                    { name: "Pack 1 (Défi SVT): Génétiques des diploïdes", link: "#" },
-                    { name: "Pack 2 (Défi SVT): Génétiques des diploïdes", link: "#" },
-                    { name: "Pack 1 (Défi SVT): Génétique Humaine", link: "#" },
-                    { name: "Pack 2 (Défi SVT): Génétique Humaine", link: "#" }
-                ],
-                "Neurophysiologie": [
-                    { name: "Pack 1 (Défi SVT): Tissu Nerveux", link: "#" },
-                    { name: "Pack 1 (Défi SVT): Réflexe myotatique", link: "#" },
-                    { name: "Pack 2 (Défi SVT): Réflexe myotatique", link: "#" },
-                    { name: "Pack 1 (Défi SVT): Fonctionnement du muscle squelettique", link: "#" },
-                    { name: "Pack 1 (Défi SVT): Régulation de la pression artérielle", link: "#" },
-                    { name: "Pack 1 (Défi SVT): Hygiéne du système nerveux", link: "#" }
-                ],
-                "Immunite": [
-                    { name: "Pack 1 (Défi SVT): Réponse immunitaire spécifique", link: "#" },
-                    { name: "Pack 2 (Défi SVT): Réponse immunitaire spécifique", link: "#" },
-                    { name: "Pack 1 (Défi SVT): Allergies", link: "#" },
-                    { name: "Pack 2 (Défi SVT): Allergies", link: "#" },
-                    { name: "Pack 1 (Défi SVT): SIDA", link: "#" }
-                ]
-            };
+ const testLinks = {
+        "Reproduction Humaine": [
+            { name: "Pack 1 (Défi SVT): Chez l'Homme", link: "seek1" },
+            { name: "Pack 2 (Défi SVT): Chez l'Homme", link: "#" },
+            { name: "Pack 1 (Défi SVT): Chez la Femme", link: "#" },
+            { name: "Pack 2 (Défi SVT): Chez la Femme", link: "#" },
+            { name: "Pack 1 (Défi SVT): Procréation", link: "#" },
+            { name: "Pack 2 (Défi SVT): Procréation", link: "#" }
+        ],
+        "Evolution Biologique": [
+            { name: "Pack 1 (Défi SVT): Arguments", link: "#" },
+            { name: "Pack 2 (Défi SVT): Arguments", link: "#" },
+            { name: "Pack 1 (Défi SVT): Mécanismes", link: "#" },
+            { name: "Pack 2 (Défi SVT): Mécanismes", link: "#" }
+        ],
+        "Genetique": [ 
+            { name: "Pack 1 (Défi SVT): Brassage de l'information Génétique", link: "#" },
+            { name: "Pack 1 (Défi SVT): Génétiques des diploïdes", link: "#" },
+            { name: "Pack 2 (Défi SVT): Génétiques des diploïdes", link: "#" },
+            { name: "Pack 1 (Défi SVT): Génétique Humaine", link: "#" },
+            { name: "Pack 2 (Défi SVT): Génétique Humaine", link: "#" }
+        ],
+        "Neurophysiologie": [
+            { name: "Pack 1 (Défi SVT): Tissu Nerveux", link: "#" },
+            { name: "Pack 1 (Défi SVT): Réflexe myotatique", link: "#" },
+            { name: "Pack 2 (Défi SVT): Réflexe myotatique", link: "#" },
+            { name: "Pack 1 (Défi SVT): Fonctionnement du muscle squelettique", link: "#" },
+            { name: "Pack 1 (Défi SVT): Régulation de la pression artérielle", link: "#" },
+            { name: "Pack 1 (Défi SVT): Hygiéne du système nerveux", link: "#" }
+        ],
+        "Immunite": [
+            { name: "Pack 1 (Défi SVT): Réponse immunitaire spécifique", link: "#" },
+            { name: "Pack 2 (Défi SVT): Réponse immunitaire spécifique", link: "#" },
+            { name: "Pack 1 (Défi SVT): Allergies", link: "#" },
+            { name: "Pack 2 (Défi SVT): Allergies", link: "#" },
+            { name: "Pack 1 (Défi SVT): SIDA", link: "#" }
+        ]
+    };
